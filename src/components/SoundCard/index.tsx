56e224bd5776ac8f03c94010805d282afdc77e9d
NowDev <thefireredfs@gmail.com>
2021-12-06 15:16:29 -0300

    styling changes

--- src/components/SoundCard/index.tsx
+++ src/components/SoundCard/index.tsx
@@ -15,18 +15,26 @@ export const SoundCard: React.FC<PropsSoundCard> = props => {
   const volumeChange = (event: Event, value: number | number[]) => {
     setVolume(value as number)
   }
-  function handleClick() {
-    setEnabled(!enabled)
+  function handleClick(changeState: boolean) {
+    if (changeState) {
+      if (!enabled) setEnabled(true)
+    } else {
+      setEnabled(!enabled)
+    }
   }
 
   return (
-    <SoundCardContainer onClick={handleClick} enabled={enabled}>
+    <SoundCardContainer onClick={() => handleClick(true)} enabled={enabled}>
       <div className="cardRoot">
-        <a className="card">
+        <a className="card" onClick={() => handleClick(false)}>
           <h3>{props.title}</h3>
         </a>
       </div>
-      {enabled && <Slider aria-label="Volume" value={volume} onChange={volumeChange} />}
+      {enabled && (
+        <SliderContainer>
+          <Slider aria-label="Volume" value={volume} onChange={volumeChange} />
+        </SliderContainer>
+      )}
     </SoundCardContainer>
   )
 }
@@ -37,13 +45,28 @@ const SoundCardContainer = styled.div<{ enabled: boolean }>`
   margin-bottom: 1.5rem;
   display: flex;
   flex-direction: column;
+  cursor: ${props => (props.enabled ? 'default' : 'pointer')};
   border-style: solid;
   border-width: 0.15rem;
   border-radius: 1rem;
   border-color: ${props => (props.enabled ? '#4a63ec' : '#424242ba')};
   :hover {
     border-color: #4a63ec;
+    .card {
+      opacity: 1;
+      color: #4a63ec;
+      border-color: #4a63ec;
+      h3 {
+        color: #4a63ec;
+      }
+    }
   }
+  :focus,
+  :active {
+    color: #4a63ec;
+    border-color: #4a63ec;
+  }
+
   .cardRoot {
     width: 100%;
     height: 100%;
@@ -56,6 +79,7 @@ const SoundCardContainer = styled.div<{ enabled: boolean }>`
     align-items: center;
     width: 100%;
     opacity: 0.5;
+    opacity: ${props => (props.enabled ? '0.8' : '0.5')};
     transition: opacity 0.2s ease-in-out 0s;
     cursor: pointer;
     white-space: nowrap;
@@ -63,23 +87,17 @@ const SoundCardContainer = styled.div<{ enabled: boolean }>`
     text-overflow: ellipsis;
   }
 
-  .card:hover {
-    opacity: 1;
-    color: #4a63ec;
-    border-color: #4a63ec;
-  }
-  .card:focus,
-  .card:active {
-    color: #4a63ec;
-    border-color: #4a63ec;
-  }
-
   .card h3 {
     font-size: 1rem;
+    color: ${props => (props.enabled ? '#4a63ec' : '#424242ba')};
   }
 
   .card p {
     margin: 0;
     font-size: 0.8rem;
   }
+`
+const SliderContainer = styled.div`
+  margin: 0px 0.5rem 0px 0.5rem;
+  padding-bottom: 0.5rem;
 `
